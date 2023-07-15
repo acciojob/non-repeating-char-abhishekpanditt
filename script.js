b8c3cfeb51e36c84878136ddde7f82a41632f900
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-15 16:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function fab (str){
+  var obj = {};
+  for(let i=0;i<str.length;i++){
+    if(obj[str[i]]){
+      obj[str[i]]++;
+    }
+    else{
+      obj[str[i]] = 1;
+    }
+  }
+  for(let i =0; i<str.length;i++){
+    if(obj[str[i]]===1){
+      return str[i];
+    }
+  }
+}
